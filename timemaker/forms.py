13f3170d71ecdec55c1e4c0dd464d979c4355d0e
T.K <Taichi@TK.local>
2018-07-27 11:55:24 +0900

    second commit

--- timemaker/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
- 
- 
-class CSVUploadForm(forms.Form):
-    file = forms.FileField(label='CSVファイル', help_text='※拡張子csvのファイルをアップロードしてください。')
--- timemaker/urls.py
+++ timemaker/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = "timemaker"
 urlpatterns = [
     url(r'^index/', views.index_template, name='index_template'),
-    url(r'^upload/', views.upload, name='upload'),
     url(r'^import/', views.PostImport.as_view(), name='import'),
 ]
